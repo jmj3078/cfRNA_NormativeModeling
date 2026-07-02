525f9d809c6616cda3af7f09556a487d8b3cc009
MJCHO <jmj3078@gmail.com>
2026-07-02 22:24:26 +0900

    d

--- Modeling/run_v2_pipeline.sh
+++ Modeling/run_v2_pipeline.sh
@@ -6,7 +6,7 @@ LOG_ENGINE=run_model_engine_v2.log
 LOG_CV=cv_model_engine_v2.log
 
 echo "[$(date)] START run_model_engine_v2.py" | tee "$LOG_ENGINE"
-python run_model_engine_v2.py >> "$LOG_ENGINE" 2>&1
+python -u run_model_engine_v2.py >> "$LOG_ENGINE" 2>&1
 ENGINE_RC=$?
 echo "[$(date)] END run_model_engine_v2.py rc=$ENGINE_RC" | tee -a "$LOG_ENGINE"
 
@@ -16,7 +16,7 @@ if [ $ENGINE_RC -ne 0 ]; then
 fi
 
 echo "[$(date)] START cv_model_engine_v2.py" | tee "$LOG_CV"
-python cv_model_engine_v2.py >> "$LOG_CV" 2>&1
+python -u cv_model_engine_v2.py >> "$LOG_CV" 2>&1
 CV_RC=$?
 echo "[$(date)] END cv_model_engine_v2.py rc=$CV_RC" | tee -a "$LOG_CV"
 
